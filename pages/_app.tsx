0b96f2cc4034946b0a6cc9fffeec932e0f32844b
Pedro Pinho <rihor1000@gmail.com>
2020-12-12 10:32:09 -0300

    Removing paths aliases

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app"
 import { FC } from "react"
 
-import "@/styles/globals.css"
+import "../styles/globals.css"
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
