4dd2d6e42238efa5da9c3bc34aa770cbb3c75434
Pedro Pinho <rihor1000@gmail.com>
2020-12-15 08:58:01 -0300

    Enabling purge only on production env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  purge: [
-    "./src/components/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./src/components/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+      "./src/pages/*.{js,ts,jsx,tsx}",
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
