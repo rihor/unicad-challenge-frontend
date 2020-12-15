5d5262eeb733d1e31eeaf47033ce1cb5d78f58ae
Pedro Pinho <rihor1000@gmail.com>
2020-12-15 14:02:55 -0300

    Adding dayjs and styling SelectedDelivery

--- src/components/SelectedDelivery.tsx
+++ src/components/SelectedDelivery.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react"
+import dayjs from "dayjs"
+import { FC, useMemo } from "react"
 
 interface Props {
   delivery: Delivery
@@ -6,31 +7,52 @@ interface Props {
 }
 
 const SelectedDelivery: FC<Props> = ({ delivery, unselectDelivery }) => {
+  const formattedDate = useMemo(() => {
+    return dayjs(delivery.date).format("DD/MM/YYYY")
+  }, [delivery])
+
   return (
     <article
       role="article"
       className="w-full h-full p-6 bg-green-50 cursor-pointer">
-      <h1 className="flex items-center space-x-4">
-        <span>{delivery.start}</span>
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 8l4 4m0 0l-4 4m4-4H3"
-          />
-        </svg>
-        <span>{delivery.destination}</span>
-      </h1>
+      <div className="sm:w-8/12 w-10/12 mx-auto">
+        <h1 className="flex items-center space-x-4">
+          <span className="text-green-800 font-bold text-xl">
+            {delivery.start}
+          </span>
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17 8l4 4m0 0l-4 4m4-4H3"
+            />
+          </svg>
+          <span className="text-green-800 font-bold text-xl">
+            {delivery.destination}
+          </span>
+        </h1>
 
-      <button type="submit" onClick={unselectDelivery}>
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold text-green-800 opacity-80">
+            {delivery.client_name}
+          </h2>
+
+          <i className="text-green-800 opacity-80">{formattedDate}</i>
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        className="absolute flex items-center justify-center bottom-8 left-8 w-16 h-16 bg-red-200 z-10 rounded-full focus:outline-none"
+        onClick={unselectDelivery}>
         <svg
-          className="h-6 w-6 flex-none"
+          className="h-8 w-8 flex-none text-red-400"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -43,14 +65,6 @@ const SelectedDelivery: FC<Props> = ({ delivery, unselectDelivery }) => {
           />
         </svg>
       </button>
-
-      <div className="flex items-center justify-between">
-        <h2>{delivery.client_name}</h2>
-
-        <div>
-          <i>{delivery.date}</i>
-        </div>
-      </div>
     </article>
   )
 }
